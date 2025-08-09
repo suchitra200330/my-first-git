50840c104a32996c2a3cae686cc06b9758d35acf
suchitra <suchitrasimhadati@gmail.com>
2025-08-09 13:01:05 +0530

    Added HelloGit.java with Hello Git message

--- HelloGit.java
+++ HelloGit.java
@@ -1,1 +1,5 @@
-public class HelloGit { Public static void main(String[] args) { system.out.println("Hello Git"); }}
+public class HelloGit {
+	public static void main(String[] args) {
+		system.out.println("Hello Git");
+	}
+}
